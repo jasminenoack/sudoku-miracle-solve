5df4fd7bd1271cc4922d070d53789896db538301
Jasmine Noack <jasmine.noack@carta.com>
2022-05-27 17:48:11 -0400

    adds pencil marks

--- src/components/board/models/CellRepresentation.ts
+++ src/components/board/models/CellRepresentation.ts
@@ -1,11 +1,56 @@
+class PencilMark {
+    value: number
+    invalid: boolean = false
+
+    constructor(value: number) {
+      this.value = value
+    }
+}
+
+
 export class CellRepresentation {
-  value?: number;
+    value?: number;
+    pencilMarks: PencilMark[]
 
-  constructor(value?: number) {
-    this.value = value;
-  }
+    constructor(value?: number) {
+      this.value = value
+      this.pencilMarks = []
+    }
 
-  getValue() {
-    return this.value
-  }
+    getValue() {
+      return this.value
+    }
+
+    getPencilValues() {
+        return this.pencilMarks.map(mark => mark.value).sort()
+    }
+
+    addAllPencilMarks() {
+        this.pencilMarks = [1, 2, 3, 4, 5, 6, 7, 8, 9].map(value => new PencilMark(value))
+    }
+
+    removeAllPencilMarks() {
+        this.pencilMarks = []
+    }
+
+    addPencilMarks(values: number[]) {
+        const existingValues = this.getPencilValues()
+        values.forEach(value => {
+            if (existingValues.indexOf(value) == -1) {
+                this.pencilMarks.push(new PencilMark(value))
+            }
+        });
+    }
+
+    removePencilMarks(values: number[]) {
+        this.pencilMarks = this.pencilMarks.filter((mark) => values.indexOf(mark.value) == -1)
+    }
+
+    markPencilMarksAsInvalid(values: number[]) {
+        this.pencilMarks.forEach(mark => {
+            if (values.indexOf(mark.value) !== -1) {
+                mark.invalid = true
+            }
+        })
+    }
 }
--- src/components/board/models/tests/CellRepresentation.test.ts
+++ src/components/board/models/tests/CellRepresentation.test.ts
@@ -1,22 +1,98 @@
 import {CellRepresentation} from '../CellRepresentation'
 
+function buildCellWithValue() {
+    return new CellRepresentation(4)
+}
+
+function buildCellWithoutValue() {
+    return new CellRepresentation(undefined)
+}
+
 describe("cell", () => {
     describe("constructor", () => {
         it('builds a cell', () => {
-            const cell = new CellRepresentation(4)
+            const cell = buildCellWithValue()
             expect(cell.value).toEqual(4);
         });
     })
 
     describe('getValue', () => {
         it('returns value from cell', () => {
-            const cell = new CellRepresentation(4)
+            const cell = buildCellWithValue()
             expect(cell.getValue()).toEqual(4)
         })
 
         it('returns undefined from cell', () => {
-            const cell = new CellRepresentation(undefined)
+            const cell = buildCellWithoutValue()
             expect(cell.getValue()).toEqual(undefined)
         })
     });
+
+    describe('pencil marks', () => {
+        describe('addAllPencilMarks', () => {
+            it('adds all values as pencil marks', () => {
+                const cell = buildCellWithoutValue()
+                cell.addAllPencilMarks()
+                expect(cell.getPencilValues()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+            })
+
+            it('handles if there are already values', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 4, 6])
+                expect(cell.getPencilValues()).toEqual([1, 4, 6])
+                cell.addAllPencilMarks()
+                expect(cell.getPencilValues()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+            })
+        })
+
+        describe('addPencilMarks', () => {
+            it('adds all values as pencil marks', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 4, 6])
+                expect(cell.getPencilValues()).toEqual([1, 4, 6])
+            })
+
+            it('adds additional pencil marks', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 4, 6])
+                expect(cell.getPencilValues()).toEqual([1, 4, 6])
+                cell.addPencilMarks([2, 9])
+                expect(cell.getPencilValues()).toEqual([1, 2, 4, 6, 9])
+            })
+        })
+
+        describe('markPencilMarksAsInvalid', () => {
+            it('should mark some pencil marks as invalid', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 2, 4, 6, 8])
+                cell.markPencilMarksAsInvalid([2, 6])
+                const values = cell.pencilMarks.map(mark => [mark.value, mark.invalid])
+                expect(values).toEqual([
+                    [1, false],
+                    [2, true],
+                    [4, false],
+                    [6, true],
+                    [8, false]
+                ])
+            })
+        })
+
+        describe('removePencilMarks', () => {
+            it('removes some pencil marks', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 2, 4, 6, 8])
+                cell.removePencilMarks([2, 6])
+                expect(cell.getPencilValues()).toEqual([1, 4, 8])
+            })
+        })
+
+        describe('removeAllPencilMarks', () => {
+            it('removes all pencil marks', () => {
+                const cell = buildCellWithoutValue()
+                cell.addPencilMarks([1, 2, 4, 6, 8])
+                cell.removeAllPencilMarks()
+                expect(cell.getPencilValues()).toEqual([])
+            })
+        })
+    })
 })
